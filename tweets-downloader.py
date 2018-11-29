039a880c3c65de9b0530f4158cd695426d77bd01
Gustavo Nahum <gustavo.nahum@gmail.com>
2018-11-29 11:41:39 -0200

    Number of RT and number of # added

--- tweets-downloader.py
+++ tweets-downloader.py
@@ -20,24 +20,28 @@ def number_of_hashtags(tweetText):
                 number += 1
         return number
 
+def isRetweet(tweetText):
+        return 'RT @' in tweetText
+
 def get_msg():
         api = auth()
-        response = api.GetSearch(term='Trump',count=180,since='2018-11-28')
+        response = api.GetSearch(term='Trump',count=100,since='2018-11-22')
         
         # place the address of the output file that will receive DML commands
         fileAddress = 'C:\\Users\Gustavo\Desktop\Projeto - BD\Oficial\populate.dml'
         f = open(fileAddress,'w')
         for r in response:       
-                f.write('INSERT INTO TWEET (CREATED_AT, FAVORITE_COUNT, TWEET_ID, LANG, RETWEET_COUNT, HASHTAG_COUNT, TEXT) VALUES (' \
+                f.write('INSERT INTO TWEET (CREATED_AT, FAVORITE_COUNT, TWEET_ID, LANG, RETWEET_COUNT, IS_RETWEET, HASHTAG_COUNT, TEXT) VALUES (' \
                 + "\'" + format_date(r.__getattribute__("created_at")) + "\'" + ', ' \
                 + str(r.__getattribute__("favorite_count")) + ', ' \
                 + str(r.__getattribute__("id")) + ', ' \
                 + "\'" + r.__getattribute__("lang") + "\'" + ', ' \
                 + str(r.__getattribute__("retweet_count")) + ', ' \
+                + str(isRetweet(str(r.__getattribute__("text").replace("\n","\\n").encode("utf-8")).replace("\'", "\'\'").replace("\"", "\\\"").replace("\\'", "'"))) + ', ' \
                 + str(number_of_hashtags(str(r.__getattribute__("text").replace("\n","\\n").encode("utf-8")).replace("\'", "\'\'").replace("\"", "\\\"").replace("\\'", "'"))) + ', ' \
                 + "\'" + str(r.__getattribute__("text").replace("\n","\\n").encode("utf-8")).replace("\'", "\'\'").replace("\"", "\\\"").replace("\\'", "'") + "\'" + ");\n")
         
-                u = r.__getattribute__("user")
+                """u = r.__getattribute__("user")
                 f.write('INSERT INTO USER (CREATED_AT, DESCRIPTION, FAVOURITES_COUNT, FOLLOWERS_COUNT, FRIENDS_COUNT, ' \
                 + 'ID, LANG, LISTED_COUNT, LOCATION, NAME, SCREEN_NAME, STATUSES_COUNT, URL, VERIFIED) VALUES (' \
                 + "\'" + format_date(u.__getattribute__("created_at")) + "\'" + ', ' \
@@ -53,7 +57,7 @@ def get_msg():
                 + "\'" + str(u.__getattribute__("screen_name").encode('utf-8', errors='ignore')).replace("\'", "\'\'").replace("\"", "\\\"").replace("\\'", "'") + "\'" + ', ' \
                 + str(u.__getattribute__("statuses_count")) + ', ' \
                 + "\'" + str(u.__getattribute__("url")) + "\'" + ', ' \
-                + str(u.__getattribute__("verified")) + ');\n')
+                + str(u.__getattribute__("verified")) + ');\n')"""
         f.close()
 
 
