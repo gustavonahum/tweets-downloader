a73fc099684f889cffa4d6225c9f0cae480cefce
Gustavo Nahum <gustavo.nahum@gmail.com>
2018-11-29 08:55:03 -0200

    Date formatted correctly

--- tweets-downloader.py
+++ tweets-downloader.py
@@ -9,27 +9,30 @@ def auth():
                     access_token_secret='')
         return api
 
+def format_date(dateString):
+        monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        return dateString.split(" ")[2] + "/" + str(monthNames.index(dateString.split(" ")[1]) + 1) + "/" + dateString.split(" ")[5] + " " + dateString.split(" ")[3]
 
 def get_msg():
         api = auth()
-        response = api.GetSearch(term='Trump',count=180)
+        response = api.GetSearch(term='Trump',count=180,since='2018-11-28')
         
         # place the address of the output file that will receive DML commands
         fileAddress = 'C:\\Users\Gustavo\Desktop\Projeto - BD\Oficial\populate.dml'
         f = open(fileAddress,'w')
         for r in response:       
                 f.write('INSERT INTO TWEET (CREATED_AT, FAVORITE_COUNT, TWEET_ID, LANG, RETWEET_COUNT, TEXT) VALUES (' \
-                + "\'" + r.__getattribute__("created_at") + "\'" + ', ' \
+                + "\'" + format_date(r.__getattribute__("created_at")) + "\'" + ', ' \
                 + str(r.__getattribute__("favorite_count")) + ', ' \
                 + str(r.__getattribute__("id")) + ', ' \
                 + "\'" + r.__getattribute__("lang") + "\'" + ', ' \
                 + str(r.__getattribute__("retweet_count")) + ', ' \
-                + "\'" + str(r.__getattribute__("text").replace("\n","\\n").encode("utf-8")).replace("\'", "\'\'").replace("\"", "\\\"").replace("\\'", "'") + "\'" + ")\n")
+                + "\'" + str(r.__getattribute__("text").replace("\n","\\n").encode("utf-8")).replace("\'", "\'\'").replace("\"", "\\\"").replace("\\'", "'") + "\'" + ");\n")
         
                 u = r.__getattribute__("user")
                 f.write('INSERT INTO USER (CREATED_AT, DESCRIPTION, FAVOURITES_COUNT, FOLLOWERS_COUNT, FRIENDS_COUNT, ' \
                 + 'ID, LANG, LISTED_COUNT, LOCATION, NAME, SCREEN_NAME, STATUSES_COUNT, URL, VERIFIED) VALUES (' \
-                + "\'" + u.__getattribute__("created_at") + "\'" + ', ' \
+                + "\'" + format_date(u.__getattribute__("created_at")) + "\'" + ', ' \
                 + "\'" + str(u.__getattribute__("description").encode('utf-8', errors='ignore')).replace("\'", "\'\'").replace("\"", "\\\"").replace("\\'", "'") + "\'" + ', ' \
                 + str(u.__getattribute__("favourites_count")) + ', ' \
                 + str(u.__getattribute__("followers_count")) + ', ' \
@@ -42,7 +45,7 @@ def get_msg():
                 + "\'" + str(u.__getattribute__("screen_name").encode('utf-8', errors='ignore')).replace("\'", "\'\'").replace("\"", "\\\"").replace("\\'", "'") + "\'" + ', ' \
                 + str(u.__getattribute__("statuses_count")) + ', ' \
                 + "\'" + str(u.__getattribute__("url")) + "\'" + ', ' \
-                + str(u.__getattribute__("verified")) + ')\n')
+                + str(u.__getattribute__("verified")) + ');\n')
         f.close()
 
 
